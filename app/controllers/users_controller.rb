fe02904519c63a5a8784e4f1b6a1c208cf6a4652
Mohan Ramchandani <Ramchandani@Mohans-MacBook-Pro.local>
2017-07-06 12:59:08 +0800

    installed figaro gem

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < Clearance::UsersController
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_to welcome_path
+      # redirect_back_or url_after_create
+    else
+      render template: "users/new"
+    end
+  end
+end
