450c389f62f1df0f33d31d868d76083d5d711492
Mohan Ramchandani <Ramchandani@Mohans-MacBook-Pro.local>
2017-07-07 11:24:08 +0800

    facebook omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   get "/welcome" => "welcome#index", as: "welcome"
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'clearance/users#new'
